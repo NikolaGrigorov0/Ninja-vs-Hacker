7847974506d9457c761a4326cd84dde50cef8a4d
Klaus0447 <gabigabigavazkii@gmail.com>
2026-02-21 08:52:42 +0200

    FinalGame

--- Assets/Scripts/SwordAttack.cs
+++ Assets/Scripts/SwordAttack.cs
@@ -6,10 +6,25 @@ public class SwordAttack : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log($"SwordHitBox hit: {collision.gameObject.name}, Tag: {collision.tag}");
+        
         if (collision.CompareTag("Enemy"))
         {
-            collision.GetComponent<EnemyHealth>()?.TakeDamage(damage);
-            collision.GetComponent<BugWeakPoint>()?.TakeHit();
+            Debug.Log("Enemy detected! Attempting damage...");
+            
+            EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+                Debug.Log("Damaged enemy health!");
+            }
+            
+            BugWeakPoint bugWeakPoint = collision.GetComponent<BugWeakPoint>();
+            if (bugWeakPoint != null)
+            {
+                bugWeakPoint.TakeHit();
+                Debug.Log("Hit bug weak point!");
+            }
         }
     }
 }
